1aefbf86684f626a56e6470b2fe21b486f6b9122
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-08 08:46:12 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,5 @@
+#Escribir una función a la que se le pase una cadena <nombre> y muestre por
+#pantalla el saludo ¡hola <nombre>!.
+def bienvenida (nombre):
+    print("Bienvenido",nombre)
+bienvenida("David Gomara")
