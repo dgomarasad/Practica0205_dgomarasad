1fbd3fd0517794d86974f16e28b3a5cfaaec73b9
dgomarasad <dgomarasad@educacion.navarra.es>
2024-11-11 22:18:40 +0100

    Update Ejercicio04.py

--- Ejercicio04.py
+++ Ejercicio04.py
@@ -1,8 +1,13 @@
 #Escribir una función que reciba una muestra de números en una lista y
 #devuelva su media.
 def calcular_media(numeros):
+    """La definicion nos dice la media de los numeros de la lista
+       parametros:
+       entrada:lista de numeros que queremso que nos haga la media
+       salida:media de la lista de numeros"""
+      
     return sum(numeros) / len(numeros)
 
-"""Ahora creamos la lista con """
+
 muestra = [1,2,3,4,5,6,7]
 print("La media es:", calcular_media(muestra))
